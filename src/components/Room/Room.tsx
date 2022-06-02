fc31db4ff8b41ef6c5a1686d0da3d2a923c12f29
kolserdav <serega12101983@gmail.com>
2022-06-02 16:16:39 +0700

    Set up eslint

---
+++ src/components/Room/Room.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import s from './Room.module.scss';
+
+function Room() {
+  return (
+    <div className={s.wrapper}>
+      <video />
+    </div>
+  );
+}
+
+export default Room;
